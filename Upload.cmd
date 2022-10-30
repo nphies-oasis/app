5a08155bc762d59792bb8e10f3ef9cccc4791360
nphies-oasis <nphies.oasis@gmail.com>
2022-10-30 12:56:06 +0200

    First commit

--- Upload.cmd
+++ Upload.cmd
@@ -1,6 +1,6 @@
-TortoiseProc.exe /command:update /path:"C:\balsam\Nphies_Deploy\app\jar\NPHIESIntergation-0.0.1-SNAPSHOT.jar" /closeonend:1
-TortoiseProc.exe /command:update /path:"C:\balsam\Nphies_Deploy\app\jar\prop\application.properties" /closeonend:1
-TortoiseProc.exe /command:update /path:"C:\balsam\Nphies_Deploy\app\jar\prop\facility.properties" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\balsam\Jars\app\jar\NPHIESIntergation-0.0.1-SNAPSHOT.jar" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\balsam\Jars\app\jar\prop\application.properties" /closeonend:1
+TortoiseProc.exe /command:update /path:"C:\balsam\Jars\app\jar\prop\facility.properties" /closeonend:1
 cd C:\balsam\Nphies_Deploy\app\jar\
 git commit
 git push
