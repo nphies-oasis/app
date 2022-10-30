a41ad4f59265e1687ccfcafc7a9ea21c8990f182
nphies-oasis <nphies.oasis@gmail.com>
2022-10-30 12:59:16 +0200

    Update Upload.cmd

--- Upload.cmd
+++ Upload.cmd
@@ -1,6 +1,6 @@
 TortoiseProc.exe /command:update /path:"C:\balsam\Jars\app\jar\NPHIESIntergation-0.0.1-SNAPSHOT.jar" /closeonend:1
 TortoiseProc.exe /command:update /path:"C:\balsam\Jars\app\jar\prop\application.properties" /closeonend:1
 TortoiseProc.exe /command:update /path:"C:\balsam\Jars\app\jar\prop\facility.properties" /closeonend:1
-cd C:\balsam\Nphies_Deploy\app\jar\
+cd C:\balsam\Jars\app\
 git commit
 git push
