9abc562c62ff5a611127644707348d54588e0f17
nphies-oasis <nphies.oasis@gmail.com>
2022-11-06 09:35:14 +0200

    Referral/Offline Auth Fix

--- UpdateTestService.cmd
+++ UpdateTestService.cmd
@@ -1,6 +1,7 @@
+set YYYYMMDD=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
 set AppConfig="TEST"
 net stop OasisNphiesIntegration_%AppConfig%
-copy C:\oasis_int\nphies\OasisNphiesIntegration%AppConfig%.jar C:\oasis_int\nphies\Backup
+copy C:\oasis_int\nphies\OasisNphiesIntegration%AppConfig%.jar C:\oasis_int\nphies\Backup\OasisNphiesIntegration%AppConfig%_%YYYYMMDD%.jar
 copy C:\balsam\Nphies_Deploy\app\branch\NPHIESIntergation-0.0.1-SNAPSHOT.jar C:\oasis_int\nphies\OasisNphiesIntegration%AppConfig%.jar
 rem copy C:\oasis_int\nphies\application_%AppConfig%.properties C:\oasis_int\nphies\Backup
 rem copy C:\balsam\Nphies_Deploy\app\branch\prop\application.properties C:\oasis_int\nphies\application_%AppConfig%.properties
